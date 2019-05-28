1c9b9a4a45f564d0d5e8db31d36f692e31b6a65f
obl <kmagrawal.13@gmail.com>
2019-05-28 14:48:35 +0530

    dynamic time added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+
+  logonTime =  new Date();
+
+  currentDate = `${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`
+  currentTime =    new Date().toLocaleString('en-US', { hour: 'numeric', minute:'numeric', hour12: true })
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+
+    this.startClock();
+  }
+
+  startClock(){
+
+    setInterval(() => {
+      this.currentDate = `${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`
+      this.currentTime =    new Date().toLocaleString('en-US', { hour: 'numeric', minute:'numeric', hour12: true })
+
+    }, 1000);
   }
 
 }
